40280a1ea4210962d854cbeda00cc6f24c45184e
danbaguley <danbaguley@outlook.com>
2025-01-17 09:59:11 +0000

    Finished day 1 task

--- program/main.py
+++ program/main.py
@@ -1,1 +1,23 @@
 from room import Room
+
+kitchen = Room("Kitchen")
+ballroom = Room("Ballroom")
+
+
+
+
+kitchen = Room("kitchen")
+kitchen.set_description("A dank and dirty room buzzing with flies")
+ballroom = Room("ballroom")
+ballroom.set_description("A vast room with a shiny wooden floor")
+dining_hall = Room("dining hall")
+dining_hall.set_description("A large room with ornate golden decorations")
+
+#print(kitchen.get_description())
+
+kitchen.link_room(dining_hall, "south")
+dining_hall.link_room(kitchen, "north")
+dining_hall.link_room(ballroom,"west")
+ballroom.link_room(dining_hall,"east")
+
+dining_hall.get_details()
