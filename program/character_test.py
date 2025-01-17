24b5f3dc19e43c2e665fd931fd438579b5c5cd5b
danbaguley <danbaguley@outlook.com>
2025-01-17 10:36:38 +0000

    added testing file for character, added enemy class

---
+++ program/character_test.py
@@ -0,0 +1,8 @@
+from character import Character
+dave = Character("Dave", "A smelly zombie")
+
+dave.describe()
+
+dave.set_conversation("Hello there! I am going to join your OOP game very soon")
+
+dave.talk()
